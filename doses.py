44fb5e3a04b6639b31e00bae9f565c7a7b6e30ca
Pax Spacegoat <zarigx@gmail.com>
2017-10-10 15:33:51 -0700

    Added "Time since last dose" function B
    B
    B
    C
    C
    
    A

--- doses.py
+++ doses.py
@@ -1,6 +1,8 @@
 import json;
 import time;
 from use import dose;
+import math;
+
 class usage:
     doses=[]
     def add_dose(self,d):
@@ -10,15 +12,18 @@ class usage:
         return self.doses.count()
     def get_dose_num(self,num):
         return self.doses[num]
-    def get_from_json_file(self, filename):
-        with open(filename, 'r') as fi:
-            return (fi.read().splitlines())
-            fi.close()
-    def get_last_time(self):
-        dos = self.get_from_json_file("usage.json")
-        do = dose(dos[0])
-        print (do)
-        return dos[0]
+    def get_from_json_file(self):
+       with open("usage.json") as inf:
+        lines = inf.readlines();
+        for dline in lines:
+            dstr = str(dline)
+            dobj=json.loads(str(dstr))
+            d = dose(dobj['mg'],dobj['analgesic'],dobj['halflife'],dobj['time'])
+            self.add_dose(d)
+    def get_last_dose(self):
+        self.get_from_json_file()
+        dos = self.doses[0]
+        return dos
     def save_to_json(self, filename):
         with open(filename,'w') as wf:
          for use in self.doses:
@@ -32,7 +37,16 @@ class usage:
 
 def get_last_time():
     my_usage=usage()
-    my_usage.get_last_dose()
+    my_usage.get_from_json_file()
+    dos = my_usage.get_last_dose()
+    difsecs = (float(time.time()-float(dos.time)))
+    difsecs = math.floor(difsecs)
+    deltasecs = difsecs % 60
+    deltamin = math.floor(difsecs/60) % 60
+    deltahours = math.floor(difsecs/3600)
+    print (str(deltahours) + ":" + str(deltamin) + ":" + str(deltasecs))
+
+
 def use(d_mg='150', d_str='1'):
     my_usage=usage()
     d=(dose(d_mg,d_str,'1',str(time.time())))
@@ -46,3 +60,4 @@ def use(d_mg='150', d_str='1'):
             d = dose(dobj['mg'],dobj['analgesic'],dobj['halflife'],dobj['time'])
             my_usage.add_dose(d)
     my_usage.save_to_json("usage.json")
+
